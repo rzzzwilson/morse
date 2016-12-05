b311409cc33351d0cc7967d587cff3f8c961679a
Ross Wilson <rzzzwilson@gmail.com>
2016-12-05 11:41:22 +0700

    Started a utility module

---
+++ morse_trainer/utils.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+Small utility functions.
+"""
+
+
+def morse2display(morse):
+    """Convert a string for a morse character to 'display' morse.
+
+    We use this to display expected/received characters in the
+    display as using ./- is not nice.  For example, '.-.' is not
+    as nice as '• ━ •'
+    """
+
+    # unicode chars
+    DOT = '•'    #2022
+    DASH = '━'   #2501
+    SIX_PER_EM_SPACE = ' '   #2005
+
+    result = []
+    for dotdash in morse:
+        if dotdash == '.':
+            result.append(DOT)
+        elif dotdash == '-':
+            result.append(DASH)
+        else:
+            raise Exception("Unrecognized sign in '%s': %s" % (morse, dotdash))
+
+    return SIX_PER_EM_SPACE.join(result)
+
+
+if __name__ == '__main__':
+    morse = '.'
+    while morse:
+        morse = input('Enter morse, nothing to exit: ')
+        result = morse2display(morse)
+        print(result)
