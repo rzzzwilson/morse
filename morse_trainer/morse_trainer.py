dc0587a80441db1fd2c5a2ad572c0c404177a5af
Ross Wilson <rzzzwilson@gmail.com>
2016-12-06 10:28:29 +0700

    Better logging and thread closing

--- morse_trainer/morse_trainer.py
+++ morse_trainer/morse_trainer.py
@@ -16,6 +16,8 @@ from PyQt5.QtWidgets import QApplication, QHBoxLayout, QVBoxLayout, QWidget
 from PyQt5.QtWidgets import QPushButton, QMessageBox
 import morse
 import display
+import logger
+log = logger.Log('debug.log', logger.Log.DEBUG)
 
 
 # set platform-dependent stuff, if any
@@ -33,7 +35,7 @@ else:
 class Communicate(QObject):
     """Signal/slot communication class."""
 
-    morseChar = pyqtSignal('QString')       # received morse chars
+    morse_char = pyqtSignal('QString')       # received morse char
 
 
 class MorseReader(QThread):
@@ -52,28 +54,40 @@ class MorseReader(QThread):
         super().__init__()
 
         self.sig_obj = sig_obj
-        print('self.sig_obj=%s' % type(self.sig_obj))
         self.params_file = params_file
         self.running = False
 
         self.morse = morse.ReadMorse()
         if self.params_file:
+            log('Loading params from %s' % self.params_file)
             self.morse.load_params(self.params_file)
 
-    def __del__(self):
-        # close morse reader
-        self.running = False
-        self.wait()
+#    def __del__(self):
+#        log('__del__')
+#        # save updated params
+#        if self.params_file:
+#            log('Saving params to %s' % self.params_file)
+#            self.morse.save_params(self.params_file)
+#        # close morse reader
+#        self.running = False
+#        self.wait()
+
+    def close(self):
         if self.params_file:
+            log('Saving params to %s' % self.params_file)
             self.morse.save_params(self.params_file)
+        log('Stopping thread')
+        self.running = False
+        self.wait(500)
+        log('After .wait(500)')
 
     def run(self):
         self.running = True
-        print('run() looping')
         while self.running:
             char = self.morse.read_morse()
-            print('Sending: %s' % char)
-            self.sig_obj.morseChar.emit(char)
+            log('found: %s' % char)
+            if len(char) == 1:
+                self.sig_obj.morse_char.emit(char)
 
 
 class MorseTrainer(QWidget):
@@ -86,74 +100,37 @@ class MorseTrainer(QWidget):
 
         # create signal/slot for recognized morse characters
         self.got_morse_char = Communicate()
-        self.got_morse_char.morseChar.connect(self.morseChar)
+        self.got_morse_char.morse_char.connect(self.gotMorseChar)
 
         self.read_thread = MorseReader(self.got_morse_char, params_file)
         self.read_thread.start()
 
+        # populate the display widget a bit
+        self.display.insert_upper('U', fg=display.Display.AskTextColour)
+
     def initUI(self):
         self.display = display.Display()
-        left_button = QPushButton('Left ⬅', self)
-        right_button = QPushButton('Right ➡', self)
 
-        hbox1 = QHBoxLayout()
-        hbox1.addWidget(self.display)
+        hbox = QHBoxLayout()
+        hbox.addWidget(self.display)
 
-        hbox2 = QHBoxLayout()
-        hbox2.addWidget(left_button)
-        hbox2.addWidget(right_button)
-
-        vbox = QVBoxLayout()
-        vbox.addLayout(hbox1)
-        vbox.addLayout(hbox2)
-        self.setLayout(vbox)
-
-        left_button.clicked.connect(self.leftButtonClicked)
-        right_button.clicked.connect(self.rightButtonClicked)
+        self.setLayout(hbox)
 
         self.setGeometry(100, 100, 800, 200)
         self.setWindowTitle('Example of Display widget')
         self.show()
 
-        # populate the display widget a bit
-        for index in range(30):
-            if index in (7, 21):
-                self.display.insert_upper('U', fg=display.Display.AnsTextBadColour)
-            else:
-                self.display.insert_upper('U', fg=display.Display.AskTextColour)
-
-        for index in range(29):
-            if index in (5, 19):
-                self.display.insert_lower('L', fg=display.Display.AnsTextBadColour)
-            else:
-                self.display.insert_lower('L', fg=display.Display.AnsTextGoodColour)
-        self.display.set_highlight()
-        self.display.set_tooltip(0, "Expected 'A', got 'N'\nweqweqwe")
-        self.display.set_tooltip(5, 'Tooltip at index 5, rwtrwtrewtrwtrewtrewtrewtrewtrewtrewtrewtrewtrewtrewtrewtrewtrew')
-        self.display.set_tooltip(19, "Expected 'A', got 'N'\nweqweqwe\nasdasdadasd\na\na\na\na\na")
-
-    def morseChar(self, char):
+    def gotMorseChar(self, char):
         """The morse reader thread found a character."""
 
-        self.display.insert_lower(char)
-        self.display.insert_upper('.')
+        self.display.insert_upper('U')
+        self.display.insert_lower(char, fg=display.Display.AnsTextBadColour)
         self.display.set_highlight()
         self.display.update()
 
-    def leftButtonClicked(self):
-        """Move highlight to the left, if possible."""
-
-        index = self.display.get_highlight()
-        if index is not None:
-            index -= 1
-            if index >= 0:
-                self.display.set_highlight(index)
-
-    def rightButtonClicked(self):
-        """Simulate getting a morse char."""
-
-        self.got_morse_char.morseChar.emit('A')
-
+    def closeEvent(self, event):
+        log('Closing read_thread')
+        self.read_thread.close()
 
 
 # get program name from sys.argv
@@ -166,5 +143,4 @@ params_file = '%s.param' % prog_name
 
 app = QApplication(sys.argv)
 ex = MorseTrainer(params_file)
-app.exec()
-#sys.exit(app.exec_())
+sys.exit(app.exec())
