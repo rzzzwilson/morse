0cadc978f6649e88ccd52c23e8682ce673009f3b
Ross Wilson <rzzzwilson@gmail.com>
2016-12-12 17:47:25 +0700

    Final form

--- morse_trainer/test_receive_group.py
+++ morse_trainer/test_receive_group.py
@@ -2,40 +2,44 @@
 # -*- coding: utf-8 -*-
 
 """
-Test of grouping - make the receive boxgroup.
+Code to test the 'Receive' group.
 """
 
-from PyQt5.QtCore import pyqtSignal
-from PyQt5.QtWidgets import QApplication, QWidget, QRadioButton
-from PyQt5.QtWidgets import QHBoxLayout, QVBoxLayout, QGridLayout, QGroupBox
+from PyQt5.QtWidgets import *
 
-
-class GroupBox(QWidget):
-
+class ReceiveGroup(QWidget):
     def __init__(self):
         QWidget.__init__(self)
-        self.setWindowTitle('Test of Receive group control')
+        self.setWindowTitle('ReceiveGroup Test')
+        self.initUI()
 
-        layout = QGridLayout()
-        self.setLayout(layout)
+    def initUI(self):
+        # define the widgets in this group
+        combo = QComboBox(self)
+        combo.addItem('No grouping')
+        combo.addItem('3 characters')
+        combo.addItem('4 characters')
+        combo.addItem('5 characters')
+        combo.addItem('6 characters')
+        combo.addItem('7 characters')
+        combo.addItem('8 characters')
+        label = QLabel('Groups:')
 
-        groupbox = QGroupBox("Receive")
-#        groupbox.setCheckable(True)
-        layout.addWidget(groupbox)
+        hbox = QHBoxLayout()
+        hbox.addWidget(label)
+        hbox.addWidget(combo)
+        hbox.addStretch(1)
 
         vbox = QVBoxLayout()
-        groupbox.setLayout(vbox)
+        vbox.addLayout(hbox)
 
-        radiobutton = QRadioButton("Characters")
-        radiobutton.setChecked(True)
-        vbox.addWidget(radiobutton)
-        radiobutton = QRadioButton("Groups")
-        vbox.addWidget(radiobutton)
+        self.setLayout(vbox)
+
 
 if __name__ == '__main__':
     import sys
 
     app = QApplication(sys.argv)
-    screen = GroupBox()
+    screen = ReceiveGroup()
     screen.show()
-sys.exit(app.exec())
+    sys.exit(app.exec())
